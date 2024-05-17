6e60d569a91df3f9839e07cfae7562e36e140f70
Scarface8080 <hamzashoukat8080@gmail.com>
2024-05-18 03:27:34 +0500

    dixed some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Education from "./components/Education";
